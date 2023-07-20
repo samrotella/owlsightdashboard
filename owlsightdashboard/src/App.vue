6bdb17a5faa197eed7f75b95277bfd003b172a45
samrotella <48039441+samrotella@users.noreply.github.com>
2023-07-20 12:27:42 -0400

    cleanup

--- owlsightdashboard/src/App.vue
+++ owlsightdashboard/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import Login from './components/Login.vue'
 </script>
 
 <template>
@@ -8,15 +8,15 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <Login msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/">Dashboard</RouterLink>
       </nav>
     </div>
   </header>
 
+  <!-- eslint-disable-next-line -->
   <RouterView />
 </template>
 

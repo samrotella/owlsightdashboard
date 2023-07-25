4dad05a1e27979b6224997b7556c1f1b88eaf98c
samrotella <48039441+samrotella@users.noreply.github.com>
2023-07-25 12:33:59 -0400

    live data

--- owlsightdashboard/src/store/modules/data.js
+++ owlsightdashboard/src/store/modules/data.js
@@ -5,8 +5,9 @@ export const data = reactive({
     uniqueCount: null,
     getUniqueCount() {
         return new Promise((resolve, reject) => {
+        // Axios.get(`http://localhost:3000/totalUniqueVisits`).then((response) => {
         Axios.get(`https://owlsight-api.onrender.com/totalUniqueVisits`).then((response) => {
-            this.uniqueCount = response.data;
+            this.uniqueCount = response.data.length;
             resolve(response.data);
             }).catch((error) => {
                 reject(error);
